58e800d535482416f1878bcf1b73290af688aae4
Brian Miranda <brianmiranda.dev@gmail.com>
2023-07-02 12:25:12 -0400

    Starting the goal app
    Added padding so the text doenst go under the OS status bar on the top.
    Theres better ways to do it

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>Hello World</Text>
-      <Text style={styles.dummyText}>Welcom to your app!</Text>
-      <Button title="Clicke me!" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 10,
-    padding: 10,
-    borderColor: 'blue',
-    borderWidth: 2,
+  appContainer: {
+    padding: 50,
   },
 });
