2f25a6b9eee88f9ae2b62651ad14af004627d01c
Brian Miranda <brianmiranda.dev@gmail.com>
2023-07-02 12:17:27 -0400

    Add styling
    To use styles we use inline styles or the StyleSheet object. No need to
    pass 'px'.

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.dummyText}>Hello World</Text>
+      <Text style={styles.dummyText}>Welcom to your app!</Text>
+      <Button title="Clicke me!" />
     </View>
   );
 }
@@ -16,5 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 10,
+    padding: 10,
+    borderColor: 'blue',
+    borderWidth: 2,
   },
 });
