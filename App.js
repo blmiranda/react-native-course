5dbf2cfc0c736630ca8426bcdd60c58b5369c428
Brian Miranda <brianmiranda.dev@gmail.com>
2023-07-02 14:33:18 -0400

    Add list items styles
    Some properties cant be used in both IOS and Android, so we must adapt.
    Also, theres no cascading styles, we must add the style we want where it
    should actually go

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+        <TextInput
+          placeholder="Your course goal!"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
 
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => (
+          <View style={styles.goal} key={goal}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -17,6 +40,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    backgroundColor: '#f2f2f2',
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
@@ -39,5 +63,26 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goal: {
+    backgroundColor: '#ffffff',
+    width: '100%',
+    marginTop: 10,
+    paddingVertical: 25,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    shadowRadius: 3,
+    shadowOpacity: 0.2,
+    shadowColor: '#000000',
+    elevation: 8,
+  },
+  goalText: {
+    color: '#808080',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
